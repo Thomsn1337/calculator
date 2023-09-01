b72edf8e655451e17028affd7ff4aa1e7965d457
Thomas Steifler <steiflerthomas@gmail.com>
2023-09-01 18:24:11 +0200

    Handle operator input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+// Variable definitions
 let numberInput = "0";
+let firstNumber = 0;
+let secondNumber = 0;
+let solution = 0;
+let operator = "";
+let operatorPressed = false;
 
+// DOM elements
 const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
 const commaButton = document.querySelector("#comma");
 const negateButton = document.querySelector("#negate");
 const screenInputVal = document.querySelector("#value");
+const screenEquationVal = document.querySelector("#equation");
 
 screenInputVal.textContent = numberInput;
 
+// Function definitions
+const handleOperatorClick = e => {
+  firstNumber = Number(numberInput);
+  operator = e.target.textContent;
+
+  screenEquationVal.textContent = `${firstNumber} ${operator}`
+  numberInput = "0";
+  screenInputVal.textContent = numberInput;
+}
+
+// Event listeners
 numberButtons.forEach(button => {
   button.addEventListener("click", e => {
     if (numberInput.charAt(0) === "0") numberInput = e.target.textContent;
@@ -27,3 +47,8 @@ negateButton.addEventListener("click", () => {
   else numberInput = "-" + numberInput;
   screenInputVal.textContent = numberInput;
 })
+
+operatorButtons.forEach(button => {
+  button.addEventListener("click", handleOperatorClick);
+})
+
